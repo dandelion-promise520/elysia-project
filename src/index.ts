d2c7eda1c17982a1a124b475f935a52d007903ac
dandelion <1993466159@qq.com>
2026-01-18 23:09:32 +0800

    +supabase

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-import { Elysia } from "elysia";
-import { cors } from "@elysiajs/cors";
+import { Elysia, t } from "elysia";
+import { openapi, fromTypes } from "@elysiajs/openapi";
+import { supabasePlugin } from "./plugin/supabase";
 
-const app = new Elysia()
-  .get("/", () => "Hello Elysia")
-  .use(cors())
+export const app = new Elysia()
+  .use(
+    openapi({
+      references: fromTypes(),
+      path: "/docs",
+    }),
+  )
+  .use(supabasePlugin)
+
+  .get("/", () => {
+    return { test: "hello" as const };
+  })
+  .get("/product", async ({ supabase }) => {
+    const { data, error } = await supabase.from("product").select("*");
+
+    if (error) return { error: error.message };
+    return { product: data };
+  })
+
+  .post("/json", ({ body, status }) => body, {
+    body: t.Object({
+      hello: t.String(),
+    }),
+  })
+
   .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
